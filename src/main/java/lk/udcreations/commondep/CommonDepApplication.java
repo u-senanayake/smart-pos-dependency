6121c96bbd23147033161384397dff5be687303b
Udayanga Senanayake <mailtoudayanga@gmail.com>
2025-03-17 07:15:11 +0000

    updated common-lib project

---
+++ src/main/java/lk/udcreations/commondep/CommonDepApplication.java
@@ -0,0 +1,13 @@
+package lk.udcreations.commondep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonDepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonDepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/udcreations/commondep/CommonDepApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.udcreations.commondep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
